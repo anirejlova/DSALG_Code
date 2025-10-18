3d8a0d09a2960567491d2b5a4c6b4c912ab986c2
anirejlova <anna8080@post.cz>
2025-10-19 00:37:52 +0100

    add dalins code (complex for no ahh reason), exc2 stacks

---
+++ w1-Dalin.py
@@ -0,0 +1,118 @@
+class Queue:
+    def __init__(self):
+        self.data = []
+        self.head = 0
+        self.tail = -1
+        self.max_size = 15
+
+    def enqueue(self, newData):
+        print("Enqueue the data " + str(newData))
+        if self.tail == self.max_size - 1:
+            raise OverflowError("Cannot insert into a full queue.")
+        self.tail += 1
+        self.data.insert(self.tail, newData)
+        print("Now the tail index is " + str(self.tail))
+
+    def dequeue(self):
+        if self.tail == -1:
+            raise IndexError("Cannot delete from an empty queue.")
+        removed = self.data[self.head]
+        print("Dequeue the data " + str(removed))
+        temp = self.head
+        while temp < self.tail:
+            temp += 1
+            self.data[temp - 1] = self.data[temp]
+        del self.data[temp]
+        self.tail -= 1
+        return removed
+
+    def peek(self):
+        if self.tail == -1:
+            raise IndexError("Cannot peek from an empty queue.")
+        print("Peek the head")
+        return self.data[self.head]
+
+    def length(self):
+        return self.tail + 1
+
+    def print_message(self):
+        print(
+            "The queue size is "
+            + str(self.length())
+            + " The first element is "
+            + str(self.peek())
+        )
+
+    def print_all(self):
+        while self.tail != -1:
+            print(self.dequeue())
+
+
+class Stack:
+    def __init__(self):
+        self.data = []
+        self.top = -1
+        self.max_size = 5
+
+    def push(self, newData):
+        print("Push the data " + str(newData))
+        if self.top == self.max_size - 1:
+            raise OverflowError("Cannot insert into a full stack.")
+        self.top += 1
+        self.data.insert(self.top, newData)
+        print("Now the top index is " + str(self.top))
+
+    def pop(self):
+        if self.top == -1:
+            raise IndexError("Cannot delete from an empty stack.")
+        removed = self.data[self.top]
+        del self.data[self.top]
+        self.top -= 1
+        print("Pop the data " + str(removed))
+        return removed
+
+    def peek(self):
+        if self.top == -1:
+            raise IndexError("Cannot peek from an empty stack.")
+        print("Peek the top")
+        return self.data[self.top]
+
+    def length(self):
+        return self.top + 1
+
+    def print_message(self):
+        print(
+            "The stack size is "
+            + str(self.length())
+            + " The top element is "
+            + str(self.peek())
+        )
+
+    def print_all(self):
+        while self.top != -1:
+            print(self.pop())
+
+
+# run
+q = Queue()
+for j in range(10, 20):
+    q.enqueue(50 - j)
+
+q.print_message()
+for i in range(10, 16):
+    q.dequeue()
+
+q.print_message()
+q.print_all()
+
+
+s = Stack()
+for i in range(1, 9):  # raised OverflowError on 6th push <- full stack
+    s.push(i)
+
+s.print_message()
+for i in range(1, 5):  # raised IndexError on 4th pop <- empty stack
+    s.pop()
+
+s.print_message()
+s.print_all()
--- w1.py
+++ w1.py
@@ -1,7 +1,7 @@
 # Implement a Queue using an Array/List:
 class Queue:
     def __init__(self):
-        self.items = [1,2,3]
+        self.items = [1, 2, 3]
 
     # Add an element to the tail (rear) of the queue
     def enqueue(self, item):
@@ -25,11 +25,46 @@ class Queue:
             print(self.items[0])
         else:
             print("Queue is empty")
-        
+
     #  Return the number of elements in the queue
     def length(self):
         print("Length")
         print(len(self.items))
+
+
+# Implement a Stack using an Array/List:
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    # Add an element to the top of the stack
+    def push(self, item):
+        print("Push")
+        self.items.append(item)
+        print(self.items)
+
+    #  Remove the element from the top of the stack
+    def pop(self):
+        print("Pop")
+        if not len(self.items) == 0:
+            self.items.pop()
+            print(self.items)
+        else:
+            print("Stack is empty")
+
+    # View the element at the top of the stack without removing it
+    def peek(self):
+        print("Peek")
+        if not len(self.items) == 0:
+            print(self.items[-1])
+        else:
+            print("Stack is empty")
+
+    # Return the number of elements in the stack
+    def length(self):
+        print("Length")
+        print(len(self.items))
+
 
 # run
 q = Queue()
@@ -37,3 +72,11 @@ q.enqueue(4)
 q.dequeue()
 q.peek()
 q.length()
+
+s = Stack()
+print("for loop for data input:")
+for i in range(1, 5):
+    s.push(i)
+s.pop()
+s.peek()
+s.length()
