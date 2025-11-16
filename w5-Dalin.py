76e937579f79360068617625a7bd49237b25cbed
anirejlova <anna8080@post.cz>
2025-11-16 18:05:59 +0000

    w5 - delete node

--- w5-Dalin.py
+++ w5-Dalin.py
@@ -48,3 +48,42 @@ class BST:
             return self._search(curr_node.left, key)
         else:  # go to right subtree
             return self._search(curr_node.right, key)
+
+    # Traverse BST until target is found and delete it
+    # 3 cases: node has no child/subtree, one child/subtree, two children/subtrees
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, curr_node, key):
+        if curr_node is None:
+            return curr_node  # base case: tree is empty
+
+        if key < curr_node.value:  # go to left subtree
+            curr_node.left = self._delete(curr_node.left, key)
+        elif key > curr_node.value:  # go to right subtree
+            curr_node.right = self._delete(curr_node.right, key)
+        else:  # node with one or no child
+            if curr_node.left is None:
+                temp = curr_node.right
+                curr_node = None  # delete node
+                return temp
+            elif curr_node.right is None:
+                temp = curr_node.left
+                curr_node = None  # delete node
+                return temp
+            # Node with two children: get inorder successor (smalleest/leftmoset in right subtree) to replace the value of the deleted node)
+            temp = self._min_value_node(curr_node.right)
+            curr_node.value = (
+                temp.value
+            )  # copy the inorder successor's content to this node
+            curr_node.right = self._delete(
+                curr_node.right, temp.value
+            )  # delete the inorder successor
+        return curr_node
+
+    def _min_value_node(self, node):
+        # locate smallest/leftmost node in the right subtree
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
