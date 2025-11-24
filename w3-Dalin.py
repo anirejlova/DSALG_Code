27134c5ecf2ae44a7c0b34e082e4d55748d4c8ec
anirejlova <anna8080@post.cz>
2025-11-24 14:40:30 +0000

    w3 - re-do for recursion test in Dec

---
+++ w3-Dalin.py
@@ -0,0 +1,9 @@
+# RECURSION AND BACKTRACCKING
+
+from random import sample
+
+# Dev recursive and iterative alg to calc N-th fibonnaci number F(N)
+    # Fibonacci sequence is defined as F(N+2) = F(N) + F(N+1), with F(1) = F(2) = 1.
+    # F(1) and F(2) are 0 and 1 respectively.
+class Recursion:
+    
